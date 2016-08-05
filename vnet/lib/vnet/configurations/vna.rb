dfacd9689657a21d850dd884f6f908bc6ff56b55
Metallion <akieckens@axsh.net>
2016-08-05 15:51:21 +0900

    Added config options to set the ovs-ofctl and ovs-vsctl command options

--- vnet/lib/vnet/configurations/vna.rb
+++ vnet/lib/vnet/configurations/vna.rb
@@ -48,6 +48,10 @@ module Vnet::Configurations
     param :node_api_proxy, :default => :rpc
     param :trema_home, :default => Gem::Specification.find_by_name('trema').gem_dir
     param :trema_tmp, :default => '/var/run/openvnet'
+
+    param :ovs_ofctl, :default => 'ovs-ofctl -O OpenFlow13'
+    param :ovs_ofctl_10, :default => 'ovs-ofctl -O OpenFlow10'
+    param :ovs_vsctl, :default => 'ovs-vsctl'
   end
 
 end
